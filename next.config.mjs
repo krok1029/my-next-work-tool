2fc93e2858dc0c64bd33374bab10b22b466b616e
leo lee <krok1029@gmail.com>
2024-10-15 13:07:06 +0800

    PWA trytry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
