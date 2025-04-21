bd4be369a5ad97163f3baae01f716ea5e219ed04
leo lee <krok1029@gmail.com>
2025-04-21 11:14:11 +0800

    chore: rename MultipleSelector

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -19,9 +19,66 @@ const TodoDetail = () => {
       </div>
       <div className="flex flex-1 flex-col gap-y-2">
         <Label>Label</Label>
+        <MultipleSelectorWithAsyncSearchAndCreatable />
         <Label>Subtask</Label>
       </div>
     </div>
   );
 };
 export default TodoDetail;
+
+import MultipleSelector, { Option } from '@/components/ui/multiple-selector';
+import { useState } from 'react';
+
+
+const OPTIONS: Option[] = [
+  { label: 'nextjs', value: 'Nextjs' },
+  { label: 'React', value: 'react' },
+  { label: 'Remix', value: 'remix' },
+  { label: 'Vite', value: 'vite' },
+  { label: 'Nuxt', value: 'nuxt' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Ember', value: 'ember' },
+  { label: 'Gatsby', value: 'gatsby' },
+  { label: 'Astro', value: 'astro' },
+];
+
+const mockSearch = async (value: string): Promise<Option[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const res = OPTIONS.filter((option) => option.value.includes(value));
+      resolve(res);
+    }, 1000);
+  });
+};
+
+const MultipleSelectorWithAsyncSearchAndCreatable = () => {
+  const [isTriggered, setIsTriggered] = useState(false);
+
+  return (
+    <div className="flex w-full flex-col gap-5 px-10">
+      <MultipleSelector
+        onSearch={async (value) => {
+          setIsTriggered(true);
+          const res = await mockSearch(value);
+          setIsTriggered(false);
+          return res;
+        }}
+        defaultOptions={[]}
+        creatable
+        placeholder="trying to search 'a' to get more options..."
+        loadingIndicator={
+          <p className="py-2 text-center text-lg leading-10 text-muted-foreground">loading...</p>
+        }
+        emptyIndicator={
+          <p className="w-full text-center text-lg leading-10 text-muted-foreground">
+            no results found.
+          </p>
+        }
+      />
+    </div>
+  );
+};
+
